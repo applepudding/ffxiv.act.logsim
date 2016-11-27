89e1217c5c472a1efc0b7f89fa3824dc2868442b
Apple Pudding <melonpudding@live.com>
2016-11-26 21:12:57 -0800

    Initial

--- ffxiv.act.logsim/Form1.Designer.cs
+++ ffxiv.act.logsim/Form1.Designer.cs
@@ -28,12 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.list_log = new System.Windows.Forms.ListView();
+            this.lbl_timer = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.txt_fileOut = new System.Windows.Forms.TextBox();
+            this.txt_fileIn = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl_lineWrite = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // list_log
+            // 
+            this.list_log.Location = new System.Drawing.Point(64, 147);
+            this.list_log.Name = "list_log";
+            this.list_log.Size = new System.Drawing.Size(287, 175);
+            this.list_log.TabIndex = 6;
+            this.list_log.UseCompatibleStateImageBehavior = false;
+            this.list_log.View = System.Windows.Forms.View.List;
+            // 
+            // lbl_timer
+            // 
+            this.lbl_timer.AutoSize = true;
+            this.lbl_timer.Location = new System.Drawing.Point(151, 90);
+            this.lbl_timer.Name = "lbl_timer";
+            this.lbl_timer.Size = new System.Drawing.Size(49, 13);
+            this.lbl_timer.TabIndex = 4;
+            this.lbl_timer.Text = "00:00:00";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(70, 85);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(75, 23);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // txt_fileOut
+            // 
+            this.txt_fileOut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_fileOut.Enabled = false;
+            this.txt_fileOut.Location = new System.Drawing.Point(70, 59);
+            this.txt_fileOut.Name = "txt_fileOut";
+            this.txt_fileOut.Size = new System.Drawing.Size(558, 20);
+            this.txt_fileOut.TabIndex = 2;
+            this.txt_fileOut.Text = "output.txt";
+            // 
+            // txt_fileIn
+            // 
+            this.txt_fileIn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txt_fileIn.Location = new System.Drawing.Point(70, 30);
+            this.txt_fileIn.Name = "txt_fileIn";
+            this.txt_fileIn.Size = new System.Drawing.Size(477, 20);
+            this.txt_fileIn.TabIndex = 1;
+            this.txt_fileIn.Text = "input.txt";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.lbl_lineWrite);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lbl_timer);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btn_start);
+            this.groupBox1.Controls.Add(this.txt_fileOut);
+            this.groupBox1.Controls.Add(this.txt_fileIn);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(640, 139);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Config";
+            // 
+            // lbl_lineWrite
+            // 
+            this.lbl_lineWrite.AutoSize = true;
+            this.lbl_lineWrite.Location = new System.Drawing.Point(206, 90);
+            this.lbl_lineWrite.Name = "lbl_lineWrite";
+            this.lbl_lineWrite.Size = new System.Drawing.Size(13, 13);
+            this.lbl_lineWrite.TabIndex = 8;
+            this.lbl_lineWrite.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "File Output";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "File Input";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(553, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Text File|*.txt";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(640, 542);
+            this.Controls.Add(this.list_log);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "ffxiv.act.logsim";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView list_log;
+        private System.Windows.Forms.Label lbl_timer;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.TextBox txt_fileOut;
+        private System.Windows.Forms.TextBox txt_fileIn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lbl_lineWrite;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- ffxiv.act.logsim/Form1.cs
+++ ffxiv.act.logsim/Form1.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
+using System.Diagnostics; //stopwatch
+using System.Text.RegularExpressions; //regex
 
 namespace ffxiv.act.logsim
 {
@@ -15,6 +12,207 @@ namespace ffxiv.act.logsim
         public Form1()
         {
             InitializeComponent();
+        }
+        Stopwatch stopWatch = new Stopwatch();
+        private static System.Timers.Timer fightTimer;
+        public Thread myThread;
+        public bool workerRunning = false;
+        string inputFileName;
+        string outputFileName;
+        long lineCount = 0;
+
+        void log(string value)
+        {
+            if (!InvokeRequired)
+            {
+                string result = DateTime.Now + ": " + value;
+                list_log.Items.Insert(0, result);
+                while (list_log.Items.Count > 100)
+                {
+                    list_log.Items.RemoveAt(list_log.Items.Count - 1);
+                }
+            }
+            else
+            {
+                Invoke(new Action<string>(log), value);
+            }
+        }
+
+        void finishActivity()
+        {
+            if (!InvokeRequired)
+            {
+                stopWatch.Stop();
+                fightTimer.Enabled = false;
+                log("Watch stopped");
+                workerRunning = false;
+                btn_start.Text = "Start";
+                myThread.Abort();
+
+
+            }
+            else
+            {
+                Invoke(new Action(finishActivity));
+            }
+        }
+
+        void disposeStuff()
+        {
+            if (workerRunning)
+            {
+                myThread.Abort();
+            }
+            if (fightTimer != null)
+            {
+                fightTimer.Stop();
+                fightTimer.Dispose();
+                fightTimer = null;
+            }
+        } //dispose stuff
+
+        public void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            if (!InvokeRequired)
+            {
+                TimeSpan ts = stopWatch.Elapsed;
+                // Format and display the TimeSpan value.
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}", ts.Hours, ts.Minutes, ts.Seconds);
+                this.lbl_timer.Text = elapsedTime;
+                this.lbl_lineWrite.Text = lineCount.ToString();
+                //countdown handler
+            }
+            else
+            {
+                Invoke(new Action<Object, System.Timers.ElapsedEventArgs>(OnTimedEvent), source, e);
+            }
+        }
+
+
+
+        public void myBackgroudWorker()
+        {
+            using (FileStream fs = new FileStream(inputFileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    while (workerRunning)
+                    {
+                        string resultLine = sr.ReadLine();
+                        TimeSpan fightStartTime = getReadTime(resultLine);
+                        StreamWriter objWriter;
+                        while (!sr.EndOfStream) //read file till the end when first running
+                        {
+                            //C:\Users\melon\AppData\Roaming\Advanced Combat Tracker\FFXIVLogs\Network_20160823.log
+                            resultLine = sr.ReadLine();
+
+                            while ((getReadTime(resultLine) - fightStartTime).TotalSeconds > stopWatch.Elapsed.TotalSeconds)
+                            {
+                                Thread.Sleep(100);
+                            }
+
+                            //log(resultLine);
+                            lineCount++;
+                            objWriter = new StreamWriter(outputFileName, true);
+                            objWriter.WriteLine(resultLine);
+                            objWriter.Close();
+                        }
+                        objWriter = new StreamWriter(outputFileName, true);
+                        objWriter.WriteLine("=SIMSTOP=");
+                        objWriter.Close();
+                        finishActivity();
+
+                    }
+                }
+            }
+        }
+        TimeSpan getReadTime(string input_readLine)
+        {
+            string[] subname = Regex.Split(input_readLine, "\\|");
+
+            subname = Regex.Split(subname[1], "T");
+            subname = Regex.Split(subname[1], "\\-");
+            subname = Regex.Split(subname[0], "\\.");
+            subname = Regex.Split(subname[0], "\\:");
+            TimeSpan returnTs = new TimeSpan(Int32.Parse(subname[0]), Int32.Parse(subname[1]), Int32.Parse(subname[2]));
+            return returnTs;
+
+        }
+        void initStuff()
+        {
+            list_log.Dock = DockStyle.Fill;
+            fightTimer = new System.Timers.Timer(1000); // Create a timer with a 1 second interval.
+            fightTimer.Elapsed += OnTimedEvent; // Hook up the Elapsed event for the timer. 
+            fightTimer.AutoReset = true;
+            fightTimer.Enabled = false;
+
+            //delete output.txt file if exist
+            if (File.Exists("output.txt"))
+            {
+                log("Deleting old output.txt");
+                File.Delete("output.txt");
+            }
+            //create new output.txt file
+            string path = "output.txt";
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine("=logsim.init=");
+                sw.Close();
+                log("New output.txt created");
+            }            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initStuff();
+            log("App started");
+        }
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+            //this.btn_start.Text = (this.btn_start.Text == "Start") ? "Stop" : "Start";
+            if ((this.txt_fileIn.Text == "") || (this.txt_fileOut.Text == "")) return;
+
+            if (!workerRunning)
+            {
+
+                inputFileName = this.txt_fileIn.Text;
+                outputFileName = this.txt_fileOut.Text;
+                StreamWriter objWriter = new StreamWriter(outputFileName, true);
+                objWriter.WriteLine("=SIMSTART=");
+                objWriter.Close();
+                stopWatch.Reset();
+                stopWatch.Start();
+                fightTimer.Enabled = true;
+                log("Watch started");
+                workerRunning = true;
+                myThread = new Thread(myBackgroudWorker);
+                myThread.Start();
+                btn_start.Text = "Stop";
+                lineCount = 0;
+
+            }
+            else
+            {
+                finishActivity();
+                StreamWriter objWriter = new StreamWriter(outputFileName, true);
+                objWriter.WriteLine("=SIMSTOP=");
+                objWriter.Close();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            disposeStuff();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            this.txt_fileIn.Text = this.openFileDialog1.FileName;
         }
     }
 }
